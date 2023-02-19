4c48eb49b26516d92993e8104823693583f06842
Jacob <john2jo@mail.uc.edu>
2023-02-19 13:28:26 -0500

    Initial commit

---
+++ src/metric.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class metric {
+    public static void main(String[] args)
+    {
+        //Declare Variables
+        Scanner in = new Scanner(System.in);
+        double meter = 0;
+        String trash;
+
+        //Get input
+        System.out.print("Enter your measurement in meters: ");
+        if (in.hasNextDouble()) {
+            // OK safe to read in a double
+            meter = in.nextDouble();
+            in.nextLine(); // clears the newline from the buffer
+        } else {
+            // Not a double can’t use nextDouble() read as String with nextLine() instead
+            trash = in.nextLine();
+            System.out.println("You said your distance was: " + trash);
+            System.out.println("Run the program again and enter a valid amount!");
+            System.exit(0);
+
+        }
+
+        //Declare New Variables
+
+        double miles = meter / 1609.344;
+
+        double feet = meter * 3.28084;
+
+        double inches = feet * 12;
+
+        System.out.print("Your conversion is " + miles + " in miles, " + feet + " in feet, and " + inches + " in inches.");
+    }
+}
